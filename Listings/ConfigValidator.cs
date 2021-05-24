8225f471af0553769ebcd87e53db6f97b7ef7522
gierle <72036974+gierle@users.noreply.github.com>
2021-05-24 13:34:45 +0200

    Added GRASP describtion and PF excample

---
+++ Listings/ConfigValidator.cs
@@ -0,0 +1,21 @@
+// ConfigValidator
+public bool ValidateInputs(Config conf)
+	{
+		foreach (var aspect in _validationAspects)
+		{
+			aspect.Validate(conf);
+		}
+		return true;
+	}
+
+// Implementation of IValidationAspect
+public class IsCorrectCity : IValidationAspect
+    {
+        public void Validate(Config conf)
+        {
+            if (!Regex.IsMatch(conf.Location.City, @"^[a-zA-Z -']+$"))
+            {
+                throw new BadConfigException("Stadtname beinhaltet unbekannte Zeichen.");
+            }
+        }
+    }
