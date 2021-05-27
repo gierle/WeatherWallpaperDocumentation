ea86c7044627c95581696f644bc1069d6ecda1d9
leong <leon.gieringer@gmx.de>
2021-05-27 15:56:22 +0200

    GRASP, DRY and YAGNI documentation

--- Listings/ConfigValidator.cs
+++ Listings/ConfigValidator.cs
@@ -1,21 +1,22 @@
 // ConfigValidator
 public bool ValidateInputs(Config conf)
+{
+	foreach (var aspect in _validationAspects)
 	{
-		foreach (var aspect in _validationAspects)
-		{
-			aspect.Validate(conf);
-		}
-		return true;
+		aspect.Validate(conf);
 	}
+	return true;
+}
 
 // Implementation of IValidationAspect
 public class IsCorrectCity : IValidationAspect
+{
+    public void Validate(Config conf)
     {
-        public void Validate(Config conf)
+        if (!Regex.IsMatch(conf.Location.City, @"^[a-zA-Z -']+$"))
         {
-            if (!Regex.IsMatch(conf.Location.City, @"^[a-zA-Z -']+$"))
-            {
-                throw new BadConfigException("Stadtname beinhaltet unbekannte Zeichen.");
-            }
+            throw new BadConfigException("Stadtname beinhaltet unbekannte 
+                Zeichen.");
         }
     }
+}
